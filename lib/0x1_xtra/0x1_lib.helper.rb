edf305acacf810dd694a8f4fd791e580763dc79e
pima <pima@3eclipses.com>
2012-10-03 21:10:57 +0200

    corrected 0x1_lib ruby standard path

--- lib/0x1_xtra/0x1_lib.helper.rb
+++ lib/0x1_xtra/0x1_lib.helper.rb
@@ -4,7 +4,7 @@
 module X module Xtra
   X_LIB_PATH_BASE = ".0x1/00mu/00sourcing/0x1_lib"
 
-  require "#{Dir.home}/.0x1/00mu/00sourcing/0x1_lib/lib/0x1_lib/toolkit/standard.rb"
+  require "#{Dir.home}/.0x1/00mu/00sourcing/0x1_lib/lib/0x1_lib/ruby/toolkit/standard.rb"
 
 end end
 
